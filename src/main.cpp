083cf85ba172b424b0d81ae092aa28e365e2027a
David DeVore <ddevore7@gmail.com>
2021-04-17 21:55:32 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,19 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
 
+void getData() {
 
+    ifstream inFile;
+    inFile.open("data.csv");
+    string tempLine;
+    getline(inFile, tempLine);
+
+}
+
+int main() {
+
+    getData();
+    return 0;
+
+}
