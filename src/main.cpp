935d34fe118771a421c4e7983d20f43d6fc173ee
David DeVore <ddevore7@gmail.com>
2021-04-18 00:31:03 -0400

    This is the most recent main.cpp, I left some comments for myself about where to continue from where I left off.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,42 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
+#include <map>
+#include "Node.h"
+#include "Tree.h"
 using namespace std;
 
-void getData() {
+void getData() { //pass in a set as reference
 
     ifstream inFile;
     inFile.open("data.csv");
     string tempLine;
     getline(inFile, tempLine);
 
+    // make a map to use for making the nodes
+    string tempCode;
+    string tempAssignment;
+    while (!inFile.eof()) {
+
+        getline(inFile, tempCode, ',');
+
+        for (int i = 0; i < 25; i++) {
+
+            getline(inFile, tempAssignment, ',');
+            // continue from here
+
+        }
+
+    }
+
+
+
 }
 
 int main() {
 
+    // make a set
     getData();
     return 0;
 
